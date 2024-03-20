21a9c0c875c2ea37bfbfa325242bee2e42c66d8c
Chris Mitchell <cmitch419@gmail.com>
2024-03-19 21:40:35 -0400

    console bg added, still fiddly

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import { Box, Typography } from '@mui/material';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 // import Game from './components/Game';
-import BackgroundImage from '/img/bg01.jpg'
+import BackgroundImage from '/img/console.png'
 import StratagemHero from './components/StratagemHero';
 
 const App = () => {
   return (<Box className="App" sx={{
-    // backgroundImage: `url(${BackgroundImage})`,
-    // backgroundRepeat: 'no-repeat',
-    // backgroundSize: 'cover',
-    // backgroundAttachment: 'fixed',
-    backgroundColor: '#292929',
+    backgroundImage: `url(${BackgroundImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100vw',
+    height: '48.2vw',
+    display: 'flex',
+    overflow: 'hidden',
+    // backgroundColor: '#292929',
+  }}><Box sx={{
+    m: '13.5% 9.21% 13.5% 9.21%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%'
   }}>
+
       <StratagemHero />
+  </Box>
       {/* <Stratagems /> */}
       {/* <Box position="fixed" right="1rem" bottom="1rem">
         <Typography>Hold SHIFT and press W-A-S-D</Typography>
