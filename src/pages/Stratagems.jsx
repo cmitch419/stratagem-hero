688f04b10a8260ec623aba7d8ea84318ff8d726c
Chris Mitchell <cmitch419@gmail.com>
2024-03-21 13:55:31 -0400

    user can now select if gem is enabled in the list view as well

--- src/pages/Stratagems.jsx
+++ src/pages/Stratagems.jsx
@@ -6,13 +6,13 @@ import { StratagemIcon } from "../components/StratagemIcon";
 import StratagemInfo from "../components/StratagemInfo";
 import { ArrowCombo } from "../components/Stratagem";
 
-function Stratagems({stratagemsData, disabledStratagems, setDisabledStratagems}) {
+function Stratagems({ stratagemsData, disabledStratagems, setDisabledStratagems }) {
   const stratagems = stratagemsData || defaultStratagemsData;
 
   const [selectedStratagem, setSelectedStratagem] = useState(null);
-  const [categories,setCategories] = useState([]);
-window.c = categories;
-window.s = stratagems;
+  const [categories, setCategories] = useState([]);
+  window.c = categories;
+  window.s = stratagems;
   const addToDisabledStratagems = (id) => {
     setDisabledStratagems((prevDisabledStratagems) => {
       const newDisabledStratagems = new Set(prevDisabledStratagems);
@@ -29,68 +29,77 @@ window.s = stratagems;
     });
   };
 
+  function EnabledCheckbox({ stratagemId }) {
+    return (<Checkbox
+      checked={!disabledStratagems.has(stratagemId)}
+      onChange={({ target: { checked } }) => {
+        if (!checked) {
+          addToDisabledStratagems(stratagemId);
+        } else {
+          removeFromDisabledStratagems(stratagemId);
+        }
+        setSelectedStratagem(stratagemId);
+      }}
+    />);
+  }
+
   useEffect(() => {
-    console.log('stratagems:',stratagemsData);
+    console.log('stratagems:', stratagemsData);
     if (stratagemsData) { setCategories(getAllCategories(stratagemsData)) }
-  },[stratagemsData]);
+  }, [stratagemsData]);
 
   return (
     <Box sx={{ display: 'flex', background: "rgba(0,0,0,0.5)", height: '100%' }}>
       <Box sx={{ width: '40%', overflowY: 'scroll' }}>
         <List subheader={<li />}>
-          {categories.map((category,i) => {
+          {categories.map((category, i) => {
             return (<>
-            <ListSubheader disableGutters key={i} sx={{
-              display: 'flex',
-              flex: 1,
-              justifyContent: 'flex-start',
-              p: '0.25rem'
-            }}>
-              <Typography color="primary" sx={{
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-              whiteSpace: 'nowrap',
-              }}>{category}</Typography>
-            </ListSubheader>
-            {stratagems.filter(stratagem=>(stratagem.category ?? 'Uncategorized')===category).map((stratagem, index) => {
-              return (
-              // <StratagemLine key={index} stratagem={stratagem} />
-              <ListItem key={index} disablePadding>
-                <ListItemButton onClick={() => setSelectedStratagem(stratagem.id)}>
-                  <ListItemIcon>
-                    <StratagemIcon {...stratagem} />
-                  </ListItemIcon>
-                  <Stack>
-                    <ListItemText
-                      primary={stratagem.name}
-                    />
-                    <ArrowCombo {...stratagem} />
+              <ListSubheader disableGutters key={i} sx={{
+                display: 'flex',
+                flex: 1,
+                justifyContent: 'flex-start',
+                p: '0.25rem'
+              }}>
+                <Typography color="primary" sx={{
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'nowrap',
+                }}>{category}</Typography>
+              </ListSubheader>
+              {stratagems.filter(stratagem => (stratagem.category ?? 'Uncategorized') === category).map((stratagem, index) => {
+                return (
+                  <Stack key={index} direction="row">
+                    <ListItem disablePadding>
+                      <EnabledCheckbox stratagemId={stratagem.id} />
+                      <ListItemButton onClick={() => setSelectedStratagem(stratagem.id)}>
+                        <ListItemIcon>
+                          <StratagemIcon {...stratagem} />
+                        </ListItemIcon>
+                        <Stack>
+                          <ListItemText
+                            primary={stratagem.name}
+                          />
+                          <ArrowCombo {...stratagem} valid />
+                        </Stack>
+                      </ListItemButton>
+                    </ListItem>
+
                   </Stack>
-                </ListItemButton>
-              </ListItem>
-              )})}</>)})}
+                )
+              })}</>)
+          })}
         </List>
       </Box>
       <Box
-        sx={{  p: 0, width: '60%' }}
-      > 
+        sx={{ p: 0, width: '60%' }}
+      >
         <StratagemInfo stratagem={stratagemsById[selectedStratagem]} />
-        { selectedStratagem !== null
-        ? <Stack direction="row" alignItems="center">
-          <Checkbox
-            checked={!disabledStratagems.has(selectedStratagem)}
-            onChange={({target: { checked }})=>{
-              console.log(checked);
-              if (!checked) {
-                addToDisabledStratagems(selectedStratagem);
-              } else {
-                removeFromDisabledStratagems(selectedStratagem);
-              }
-            }}
-          />
-          <Typography>Enabled in Stratagem Hero</Typography>
-        </Stack>
-        : <></> }
+        {selectedStratagem !== null
+          ? <Stack direction="row" alignItems="center">
+            <EnabledCheckbox stratagemId={selectedStratagem} />
+            <Typography>Enabled in Stratagem Hero</Typography>
+          </Stack>
+          : <></>}
       </Box>
     </Box>
   );
